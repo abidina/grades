eb51f62536b3733eecdba7d28028d0a3a4efba70
abbie <abigail.c.holder@gmail.com>
2016-02-18 15:19:38 -0600

    Update grades.js

--- grades.js
+++ grades.js
@@ -5,14 +5,8 @@ var c = 0
 var b = 0
 var a = 0
 
-// Use console.log to output the following criteria:
-
-// How many of each grade?
-// What is the lowest grade?
-// What is the highest grade?
 
 var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
-// var gradesElement = document.getElementById("grades");
 
 
 for (var i = 0; i <= scores.length; i++) {
